b49a3a878a01c99664a38854a7a5aca76a6d57aa
Gahade <angrygahade@gmail.com>
2023-08-18 21:43:17 +0200

    Add generator even_odd_generator

--- main.py
+++ main.py
@@ -4,3 +4,19 @@
 які діляться на 3, "Buzz" для чисел, які діляться на 5,
 і "FizzBuzz" для чисел, які діляться як на 3, так і на 5.
 """
+
+
+def even_odd_generator(num):
+    for i in range(1, num+1):
+        if i % 3 == 0 and i % 5 == 0:
+            yield f"{i} - FizzBuzz"
+        elif i % 3 == 0:
+            yield f"{i} - Fizz"
+        elif i % 5 == 0:
+            yield f"{i} - Buzz"
+
+
+my_generator = even_odd_generator(100)
+
+for n in my_generator:
+    print(n)
