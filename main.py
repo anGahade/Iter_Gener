448cb301e9059252cb7db99c11878a3a6d91b9d1
Gahade <angrygahade@gmail.com>
2023-08-19 16:25:33 +0200

    Add iterator EvenRangeIterator

--- main.py
+++ main.py
@@ -3,14 +3,24 @@
 """
 
 
-def even_number_filter(numbers):
-    for num in numbers:
-        if num % 2 == 0:
-            yield num
+class EvenRangeIterator:
+    def __init__(self, a, b):
+        self.current = a + (a % 2)
+        self.b = b
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.current < self.b:
+            num = self.current
+            self.current += 2
+            return num
+
+        raise StopIteration
 
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-even_nums = even_number_filter(numbers)
+even_nums = EvenRangeIterator(1, 10)
 for num in even_nums:
     print(num)
 
